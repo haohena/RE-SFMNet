df6ac357ad5612ac05a462f8b552318ff67f2d4e
haohena <64673962+haohena@users.noreply.github.com>
2024-01-27 16:16:43 +0800

    Add files via upload

--- data/dataset.py
+++ data/dataset.py
@@ -11,7 +11,6 @@ import torch.nn.functional as F
 
 class Data(data.Dataset):
     def __init__(self, root, args, train=False):
-        # 返回指定路径下的文件和文件夹列表。
         self.args = args
         self.train = train
         self.imgs_HR_path = os.path.join(root, 'HR')
@@ -26,9 +25,9 @@ class Data(data.Dataset):
 
 
         self.imgs_LR = sorted(
-            glob.glob(os.path.join(self.imgs_LR_path, '*.png'))#获取指定目录下的所有图片
+            glob.glob(os.path.join(self.imgs_LR_path, '*.png'))
         )
-        # print('排序后目录下的图片：',self.imgs_LR)
+
         self.imgs_HR = sorted(
                 glob.glob(os.path.join(self.imgs_HR_path, '*.png')))
 
@@ -40,14 +39,11 @@ class Data(data.Dataset):
 
     def __getitem__(self, item): #将对象做成数组  item为对象数组下标
 
-        img_path_LR = self.imgs_LR[item]               #os.path.join(self.imgs_LR_path, self.imgs_LR[item])
-        img_path_HR = self.imgs_HR[item]               #os.path.join(self.imgs_HR_path, self.imgs_HR[item])
-        #print(img_path_LR,img_path_HR)
+        img_path_LR = self.imgs_LR[item]
+        img_path_HR = self.imgs_HR[item]
         LR = Image.open(img_path_LR)
-        #print(LR.size)
         LR = LR.resize((self.args.scale*16, self.args.scale*16), Image.BICUBIC)
         HR = Image.open(img_path_HR)
-        #print(LR.size,HR.size)
         HR = np.array(HR)
         LR = np.array(LR)
 
@@ -56,10 +52,8 @@ class Data(data.Dataset):
         HR = ToTensor()(HR)
         LR = ToTensor()(LR)
 
-        #print('HR LR shape',HR.shape,LR.shape) 128*128   128*128
         filename = os.path.basename(img_path_HR)
-        # print("lr up:",LR.size(),'img-gt:',HR.size(),'img-name:',filename)
-        return {'lr_up': LR, 'img_gt': HR, 'img_name': filename}   #这里应该要变大的？？
+        return {'lr_up': LR, 'img_gt': HR, 'img_name': filename}
 
     def __len__(self):
         return len(self.imgs_HR)
